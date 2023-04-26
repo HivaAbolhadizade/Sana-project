09b6cae3fab448636f18ff91685d942238991a49
Hiva <hivaabolhadi@gmail.com>
2023-04-26 16:58:31 +0430

    login

---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login
+
+def login_view(request):
+    return render(request, 'accounts/login.html', {})
