d14c59f9c346b0b821b49afe981e2c2d3472b2cc
Hiva <hivaabolhadi@gmail.com>
2023-04-26 16:57:13 +0430

    login

---
+++ Sana/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login
+
+def login_view(request):
+    return render(request, 'accounts/login.html', {})
